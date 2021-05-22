15c5d89b4a4184119b65c78a6bdf08ac7cc001c2
Ilya Komar <helias@yandex.ru>
2021-05-22 15:53:25 +0300

    fix: solve Sequelize and usercontroller router errors

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
-                                //database username   password
-                                const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
-                                    host: process.env.DB_HOST,
-                                    dialect: 'postgres'
-                                })
+
+//database username   password
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    operatorsAliases: false
+})
 
 sequelize.authenticate().then(
     function success() {
