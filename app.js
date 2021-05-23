2584d6878737177c899f8298b32d76191d099487
Ilya Komar <helias@yandex.ru>
2021-05-23 20:22:05 +0300

    fix: solve deprecated bodyParser bug

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const {sequelize, Sequelize} = require('./db');
 const userController = require('./controllers/usercontroller');
 const gameController = require('./controllers/gamecontroller');
@@ -12,11 +13,15 @@ const Game = GameModel(sequelize, Sequelize);
 sequelize.sync({ force: true });
 
 const app = express();
+app.use(cors());
 
-app.use(require('body-parser'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', userController);
 app.use(require('./middleware/validate-session'));
 app.use('/api/game', gameController);
+app.use((req, res) => res.status(404).send('Incorrect request!'));
+
 app.listen(port, () => {
     console.log(`Server has been started on port ${port}...`);
 })
